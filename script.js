584af56cb7ecb742af274f26b5f7b9656a0afa8c
Slimmgrimmith <dannywukasch@gmail.com>
2023-01-20 22:50:15 -0600

    Added buildRow function that creates divs in DOM

--- script.js
+++ script.js
@@ -5,14 +5,41 @@
 //const screen = document.getElementById('screen');
 
 const buildGrid = function(num) {
-    const container = document.querySelector('#screen');
-    const box = container.querySelectorAll('div');
+    let container = '';
+    container = document.getElementById('#screen');
+    let box = document.getElementById('#screen div');
 
     for(i=num;i>=0;i--){
-        document.createElement('div');
+        const div = document.createElement('div');
+        container.appendChild('div');
+        div.style.backgroundColor = 'blue';
+        div.setAttribute('style', 'width: 200px; height: 200px;');
         console.log(`div number:${i} created`);
     }
-    box.textContent = 'Hi';
 }
 
 buildGrid(9);
+console.log(container);
+
+const buildRow = function(num){
+    const square = num;
+    for(i=num;i>0;i--){
+        const buildBox = function(square){
+            for(i=num;i>0;i--){
+            const box = document.querySelector('.box');
+//My brain is powering down. I left off trying to make rows of divs to form a grid.
+//div.classList.add('new') somehow add row divs to column divs??
+            const row = 
+
+            const div = document.createElement('div');
+            div.style.flex = '1 1 auto';
+            div.style.backgroundColor = 'blue';
+            div.style.border = 'solid';
+            box.appendChild(div);
+            }
+            }
+    }
+}
+
+
+buildRow(4);
