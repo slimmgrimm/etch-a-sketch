ae930b0e7237d8038389b1a21a9c97c08c9e3c44
Slimmgrimmith <dannywukasch@gmail.com>
2023-01-24 17:28:05 -0600

    Added grid feature with CSS grid Added mouseover feature that draws black on screen

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
-//Etch a Sketch Project
-//Display a grid of divs that flex to fill a box
-//Hovering a mouse over a div changes the color of that div
+const grid = document.getElementById('grid');
 
-//const screen = document.getElementById('screen');
-
-const buildGrid = function(num) {
-    let container = '';
-    container = document.getElementById('#screen');
-    let box = document.getElementById('#screen div');
-
-    for(i=num;i>=0;i--){
-        const div = document.createElement('div');
-        container.appendChild('div');
-        div.style.backgroundColor = 'blue';
-        div.setAttribute('style', 'width: 200px; height: 200px;');
-        console.log(`div number:${i} created`);
+function createGrid(rowNumber) {
+    let gridArea = rowNumber * rowNumber;
+    grid.style.gridTemplateColumns = `repeat(${rowNumber}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${rowNumber}, 1fr)`;
+    for(let i = 0; i < gridArea; i++) {
+        let gridElement = document.createElement('div');
+        grid.appendChild(gridElement);
+        gridElement.addEventListener('mouseover', drawBlack);
+        
     }
 }
+/*const buildGrid = function(size){
+    grid.style.gridTemplateColumns = `'repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `'repeat(${size}, 1fr)`;
 
-buildGrid(9);
-console.log(container);
-
-const buildRow = function(num){
-    const square = num;
-    for(i=num;i>0;i--){
-        const buildBox = function(square){
-            for(i=num;i>0;i--){
-            const box = document.querySelector('.box');
-//My brain is powering down. I left off trying to make rows of divs to form a grid.
-//div.classList.add('new') somehow add row divs to column divs??
-            const row = 
-
-            const div = document.createElement('div');
-            div.style.flex = '1 1 auto';
-            div.style.backgroundColor = 'blue';
-            div.style.border = 'solid';
-            box.appendChild(div);
-            }
-            }
+    for(i=size*size; i>0; i--){
+        const gridElement = document.createElement('div');
+        gridElement.addEventListener('mouseover', drawBlack);
+        grid.appendChild(gridElement);
+        
+        console.log(`buildGrid looped for the ${i}th time`); 
     }
+}*/
+const drawBlack = function(e){
+    e.target.style.backgroundColor = 'black';
 }
+createGrid(100);
 
 
-buildRow(4);
+
+
+/*const buildGrid = function(gridNum){
+    const grid = document.getElementById('grid');
+    const rowNum = gridNum;
+    let colNum = gridNum;
+    for(i=rowNum;i>0;i--){
+        
+        const row = document.createElement('div');
+        grid.appendChild(row);
+        console.log(`Row looped for the ${i}th time`);
+       
+        for(i=colNum;i>0;i--){
+            const col = document.createElement('div');
+            row.appendChild(col);
+            console.log(`Col looped for the ${i}th time`);
+        }
+        
+    }
+}*/
