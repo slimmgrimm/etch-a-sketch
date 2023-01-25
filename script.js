434a3fa93352496784e00383b413679f935ed6a7
Slimmgrimmith <dannywukasch@gmail.com>
2023-01-24 21:03:15 -0600

    Added buttons to change grid size Created containers to stylize the Etch-A-Sketch aesthetic

--- script.js
+++ script.js
@@ -1,51 +1,23 @@
 const grid = document.getElementById('grid');
 
 function createGrid(rowNumber) {
+    eraseGrid();
     let gridArea = rowNumber * rowNumber;
     grid.style.gridTemplateColumns = `repeat(${rowNumber}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${rowNumber}, 1fr)`;
     for(let i = 0; i < gridArea; i++) {
         let gridElement = document.createElement('div');
         grid.appendChild(gridElement);
-        gridElement.addEventListener('mouseover', drawBlack);
-        
+        gridElement.addEventListener('mouseover', drawBlack);        
     }
 }
-/*const buildGrid = function(size){
-    grid.style.gridTemplateColumns = `'repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `'repeat(${size}, 1fr)`;
 
-    for(i=size*size; i>0; i--){
-        const gridElement = document.createElement('div');
-        gridElement.addEventListener('mouseover', drawBlack);
-        grid.appendChild(gridElement);
-        
-        console.log(`buildGrid looped for the ${i}th time`); 
-    }
-}*/
 const drawBlack = function(e){
     e.target.style.backgroundColor = 'black';
 }
-createGrid(100);
 
+function eraseGrid(){
+    grid.innerHTML = '';
+}
 
-
-
-/*const buildGrid = function(gridNum){
-    const grid = document.getElementById('grid');
-    const rowNum = gridNum;
-    let colNum = gridNum;
-    for(i=rowNum;i>0;i--){
-        
-        const row = document.createElement('div');
-        grid.appendChild(row);
-        console.log(`Row looped for the ${i}th time`);
-       
-        for(i=colNum;i>0;i--){
-            const col = document.createElement('div');
-            row.appendChild(col);
-            console.log(`Col looped for the ${i}th time`);
-        }
-        
-    }
-}*/
+createGrid(16);
